9ef407c36498003ab64d104952e6765627d265ea
Collins Uzebu <uzebuc@gmail.com>
2021-04-12 17:16:47 +0100

    reducers/action-creators

--- src/redux/slices.js
+++ src/redux/slices.js
@@ -5,7 +5,24 @@ import { getFlights } from "./thunks";
 const flightsSlice = createSlice({
   name: "flights",
   initialState: initialState.flightsState,
-  reducers: {},
+  reducers: {
+    filterByLaunchDate: (state, { payload }) => {
+      state.filters = { ...state.filters, ...payload };
+    },
+
+    filterByLaunchStatus: (state, { payload }) => {
+      state.filters = { ...state.filters, ...payload };
+    },
+
+    filterByUpcoming: (state, { payload }) => {
+      state.filters = { ...state.filters, ...payload };
+    },
+
+    removeFilter: (state, { payload }) => {
+      delete state.filters[payload.filterName];
+    },
+  },
+
   extraReducers: {
     [getFlights.pending]: (state) => {
       state.loading = true;
@@ -24,3 +41,10 @@ const flightsSlice = createSlice({
 });
 
 export const flightsReducer = flightsSlice.reducer;
+
+export const {
+  filterByLaunchDate,
+  filterByLaunchStatus,
+  filterByUpcoming,
+  removeFilter,
+} = flightsSlice.actions;
